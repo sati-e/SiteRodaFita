da7d339ac624c36bf1fa52c67cab8145a964e056
Pedro Lucas Rodrigues Greco <rgreco.pedro@dinfor.uniceub>
2024-11-06 10:22:03 -0300

    Consegui corrigir o header e fiz o esqueleto

--- src/assets/components/home.jsx
+++ src/assets/components/home.jsx
@@ -1,7 +1,8 @@
-
+import UpperPage from "./navBar/upperPage";
 
 function HomePage(){
     return (<div>
+        <UpperPage />
         <h1>Hello World</h1>
         <p>helloooooooooo</p>
     </div>);
--- src/assets/components/navBar/upperPage.jsx
+++ src/assets/components/navBar/upperPage.jsx
@@ -1,17 +1,23 @@
-import MrPop from '../../../img/mr-pop.png'
+import HomePage from "../home";
+
 
 function UpperPage(){
   return (
     <div class='Header'>
-      <div class='TitleImages'>
-          <img src={MrPop}></img>
+      <div class='SiteHeader'>
+          <a class='SiteTitle' href="../home"><h1>Roda Fita</h1></a>
+          <h2 class='ClassicCinema'>Golden age cinema</h2>
       </div>
       <div class='NavContainer'>
         <nav>
+          <a class='navItem' href='' target='self'><span>Planos</span></a>
+          <a class='navItem' href='' target='self'><span>Sobre Nós</span></a>
+          <a class='navItem' href='' target='self'><span>Cadastrar</span></a>
+          <a class='navItem' href='' target='self'><span>Entrar</span></a>
         </nav>
       </div>
     </div> 
   );
 }
 
-export default NavBar;
+export default UpperPage;
